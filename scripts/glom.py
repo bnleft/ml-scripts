cbe03b530975b1d8d48d65dd51c5929d99f2e110
Bryant Le <bryantle2021@gmail.com>
2024-03-07 13:08:04 -0600

    feat: add glom map name to id

--- scripts/glom.py
+++ scripts/glom.py
@@ -3,12 +3,18 @@ Requirements
 1. All images with the same tag must be stored in the same folder. This folder must be named like the tag name.
 2. Inside each tag folder, there are 3 sub-folders: 'Relevant annotation', 'Irrelevant', and 'Relevant'. Images must be correctly downloaded to each subfolder
 3. There are some original images which may have more than one bounding box, the region of interests inside these bounding boxes need to be extracted into sub images
+
+Edge Cases
+1. Long file names
+2. File names with illegal characters
+
 """
 
 import os
 import json
 import urllib.request
 import labelbox
+from uuid import uuid4
 
 
 def download_image(url, filepath):
@@ -21,39 +27,42 @@ def parse_ndjson(filename):
 
 
 class GlomDatasetGenerator:
-    PROJECT_ID = 'clspra3rw017b07xuf95ofdc0'
-    TASK_ID = 'clt9eyvlh0acx07wt3pk65rxy'
 
     def __init__(self, api_key):
+        self.project_id = 'clspra3rw017b07xuf95ofdc0'
+        self.task_id = 'clt9eyvlh0acx07wt3pk65rxy'
         self.client = labelbox.Client(api_key)
+        self.dataset_folder = os.path.join("datasets", "glom")
+        self.map_json = f"{self.dataset_folder}/map.json"
+
+        os.makedirs(self.dataset_folder, exist_ok=True)
+        with open(self.map_json, 'x') as file:
+            json.dump({}, file)
 
     def generate_spec(self, data_file):
         try:
-            export_task = labelbox.ExportTask.get_task(self.client, task_id=self.TASK_ID)
+            export_task = labelbox.ExportTask.get_task(self.client, task_id=self.task_id)
             export_task.get_stream(converter=labelbox.FileConverter(file_path=data_file)).start()
         except:
             print("Error downloading data, possibly due to invalid API key.")
 
     def process_data(self, data):
         for row in data:
-            dataset_folder = os.path.join("datasets", "glom")
-
             tag_value = None
 
             for field in row['metadata_fields']:
                 if field['schema_name'] == 'tag':
                     tag_value = field['value']
 
-            print(tag_value)
             if tag_value is None:
                 continue
 
             tag_value = tag_value.replace(" ", "_")
 
-            tag_folder = os.path.join(dataset_folder, tag_value)
+            tag_folder = os.path.join(self.dataset_folder, tag_value)
             os.makedirs(tag_folder, exist_ok=True)
 
-            for label in row['projects'][self.PROJECT_ID]['labels']:
+            for label in row['projects'][self.project_id]['labels']:
                 classifications = label['annotations']['classifications']
                 for classification in classifications:
                     relevant_value = classification['radio_answer']['value']
@@ -61,12 +70,27 @@ class GlomDatasetGenerator:
                     os.makedirs(relevance_folder, exist_ok=True)
 
                     file_extension = row['media_attributes']['mime_type'].split("/")[-1]
+
                     image_name = os.path.splitext(row['data_row']['external_id'])[0]
-                    image_filename = image_name + '-' + row['data_row']['id'] + '.' + file_extension
+                    image_id = row['data_row']['id']
+                    image_generated_id = self.generate_image_id(image_name)
+                    image_filename = f"{image_generated_id}-{image_id}.{file_extension}"
                     image_url = row['data_row']['row_data']
 
                     image_path = os.path.join(relevance_folder, image_filename)
                     download_image(image_url, image_path)
+
+    def generate_image_id(self, image_name: str) -> str:
+        image_id = str(uuid4())
+
+        with open(self.map_json, 'r+') as file:
+            data = json.load(file)
+            data[image_id] = image_name
+            file.seek(0)
+            json.dump(data, file, indent=4)
+            file.truncate()
+
+        return image_id
 
     def generate_dataset(self, data_file):
         data = parse_ndjson(data_file)
